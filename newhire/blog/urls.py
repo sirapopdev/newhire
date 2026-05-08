e0ac1bb511775b5b702f48973721d6253d45a63f
sirapopdev <sirapop.dev@gmail.com>
2026-05-08 09:53:01 +0700

    add list and detail page

--- newhire/blog/urls.py
+++ newhire/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostDetailView, PostListView
 
 post_list_view = PostListView.as_view()
+post_detail_view = PostDetailView.as_view()
 
 urlpatterns = [
     path('', post_list_view, name='post-list'),
+    path('<slug:slug>/', post_detail_view, name='post-detail'),
 ]
--- newhire/blog/views.py
+++ newhire/blog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from .models import Post
+
+
 # Create your views here.
 class PostListView(ListView):
     model = Post
     template_name = 'blog/list.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
